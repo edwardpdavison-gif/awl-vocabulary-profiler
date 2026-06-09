ce49765b8b5821b0d3d0016314a04d7de0967b54
edwardpdavison-gif <edwardpdavison@gmail.com>
2026-06-09 21:06:36 +0100

    Allow Netlify frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
+        "https://awl-profiler.netlify.app",
         "https://awlprofiler.blendedlearninginstitute.com",
     ],
     allow_credentials=True,
