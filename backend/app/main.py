7ceedb418d2d0af871b242894a7522b95ef6cbae
edwardpdavison-gif <edwardpdavison@gmail.com>
2026-06-09 21:19:36 +0100

    Simplify CORS for deployed frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,17 +11,11 @@ app = FastAPI(title="AWL Vocabulary Profiler")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-        "https://awl-profiler.netlify.app",
-        "https://awlprofiler.blendedlearninginstitute.com",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 awl_lookup = load_awl_data()
 gsl_lookup = load_gsl_data()
